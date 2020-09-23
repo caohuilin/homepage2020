140ef988b54aa7aefebce60461106b5adbecddba
caohiulin <1057275848@qq.com>
2020-09-24 00:01:29 +0800

    主页图片

--- components/Content/index.tsx
+++ components/Content/index.tsx
@@ -1,19 +1,31 @@
 import { Wechat } from "./wechat";
 import { Phone } from "./phone";
 import { Email } from "./Email";
+import { Computer } from "./computer";
 
-import style from './style.module.scss'
+import style from "./style.module.scss";
 
 export function Content() {
-    return (
-        <div className={style.content}>
-            <h1>I'm Huilin Cao</h1>
-            <h1>Front-end Engineer</h1>
-            <footer className={style.footer}>
-                <a><Wechat /></a>
-                <a><Phone /></a>
-                <a><Email /></a>
-            </footer>
-        </div>
-    )
+  return (
+    <div className={style.content}>
+      <div className={style.left}>
+        <h1>I'm Huilin Cao</h1>
+        <h1>Front-end Engineer</h1>
+        <footer className={style.footer}>
+          <a>
+            <Wechat />
+          </a>
+          <a>
+            <Phone />
+          </a>
+          <a>
+            <Email />
+          </a>
+        </footer>
+      </div>
+      <div className={style.right}>
+          <Computer />
+      </div>
+    </div>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
-import Head from 'next/head'
-import { Navbar } from '../components/Navbar'
-import { Content } from '../components/Content'
+import Head from "next/head";
+import { Navbar } from "../components/Navbar";
+import { Content } from "../components/Content";
 
-import style from './style.module.scss'
+import style from "./style.module.scss";
 
 export default function Home() {
   return (
@@ -10,11 +10,12 @@ export default function Home() {
       <Head>
         <title>caohuilin's HomePage</title>
         <link rel="icon" href="/favicon.ico" />
+        <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@300;400&display=swap" rel="stylesheet" />
       </Head>
       <div className={style.container}>
         <Navbar />
         <Content />
       </div>
     </div>
-  )
+  );
 }
